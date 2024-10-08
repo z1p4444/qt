450e984670238bce99c03a5d5f89b0ab107e322e
z1p4444 <1981324536@qq.com>
2024-10-08 15:24:37 +0800

    0-9

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,23 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->btnNum0,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum1,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum2,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum3,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum4,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum5,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum6,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum7,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::btnNumClicked()
+{
+    ui->statusbar->showMessage("button clicked");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,6 +16,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    void btnNumClicked();
 
 private:
     Ui::MainWindow *ui;
