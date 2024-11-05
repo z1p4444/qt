7bb764093330c9376ddf41d849f9f68a9d263539
z1p4444 <1981324536@qq.com>
2024-11-05 16:07:33 +0800

    finish main ui design

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    statusLabel.setMaximumWidth(150);
+    statusLabel.setText("length: "+QString::number(0)+"     lines: "+QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusLabel);
+
+    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setText("Ln: "+QString::number(0)+"     Col: "+QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusCursorLabel);
+
+    QLabel *author = new QLabel(ui->statusbar);
+    author->setText(tr("练子逸"));
+    ui->statusbar->addPermanentWidget(author);
 }
 
 MainWindow::~MainWindow()
