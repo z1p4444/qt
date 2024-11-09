96516507d4bceac541e6ff07cc9737e6edd53538
z1p4444 <1981324536@qq.com>
2024-11-09 16:18:21 +0800

    cut/copy/paste

--- mainwindow.h
+++ mainwindow.h
@@ -35,6 +35,22 @@ private slots:
 
     void on_textEdit_textChanged();
 
+    void on_actionUndo_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_textEdit_undoAvailable(bool b);
+
+    void on_textEdit_copyAvailable(bool b);
+
+    void on_textEdit_redoAvailable(bool b);
+
 private:
     Ui::MainWindow *ui;
     QLabel statusCursorLabel;
