c0c15acb329a5b9a9924d50a53198eb4298a8281
z1p4444 <1981324536@qq.com>
2024-12-03 16:19:35 +0800

    2024/12/3 low bat pt3 havent finished yet

--- masterview.cpp
+++ masterview.cpp
@@ -1,14 +1,88 @@
 #include "masterview.h"
 #include "ui_masterview.h"
-
+#include <QDebug>
 MasterView::MasterView(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MasterView)
 {
     ui->setupUi(this);
+
+    goLoginView();
 }
 
 MasterView::~MasterView()
 {
     delete ui;
 }
+
+void MasterView::goLoginView()
+{
+    qDebug()<<"goLoginView";
+    loginView = new LoginView(this);
+    pushWidgetToStackView(loginView);
+
+    connect(loginView,SIGNAL(loginSuccess()),this, SLOT(goWelcomeView()));
+}
+
+void MasterView::goWelcomeView()
+{
+    qDebug()<<"goWelcomeView";
+    welcomeView = new WelcomeView(this);
+    pushWidgetToStackView(welcomeView);
+}
+
+void MasterView::goDoctorView()
+{
+    qDebug()<<"goDoctorView";
+    doctorView = new DoctorView(this);
+    pushWidgetToStackView(doctorView);
+}
+
+void MasterView::goDepartmentView()
+{
+    qDebug()<<"goDepartmentView";
+    departmentView = new DepartmentView(this);
+    pushWidgetToStackView(departmentView);
+}
+
+void MasterView::goPatientEditView()
+{
+    qDebug()<<"goPatientEditView";
+    patientEditView = new PatientEditView(this);
+    pushWidgetToStackView(patientEditView);
+}
+
+void MasterView::goPatientView()
+{
+    qDebug()<<"goPatientView";
+    patientView = new patientView(this);
+    pushWidgetToStackView(patientView);
+}
+
+void MasterView::goPreviousView()
+{
+    int count = ui->stackedWidget->count();
+
+    if(count>1){
+        ui->stackedWidget->setCurrentIndex(count-2);
+        ui->labelTitle->setText(ui->stackedWidget->currentWidget()->windowTitle());
+
+        QWidget *widget=ui->stackedWidget->widget(count-1);
+        ui->stackedWidget->removeWidget(widget);
+        delete widget;
+    }
+}
+
+void MasterView::pushWidgetToStackView(QWidget *widget)
+{
+    ui->stackedWidget->addWidget(widget);
+    int count = ui->stackedWidget->count();
+    ui->stackedWidget->setCurrentIndex(count -1);
+    ui->labelTitle->setText(widget->windowTitle());
+}
+
+void MasterView::on_btBack_clicked()
+{
+    goPreviousView();
+}
+
--- masterview.h
+++ masterview.h
@@ -2,6 +2,12 @@
 #define MASTERVIEW_H
 
 #include <QWidget>
+#include "loginview.h"
+#include "doctorview.h"
+#include "departmentview.h"
+#include "patienteditview.h"
+#include "patientview.h"
+#include "welcomeview.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +22,28 @@ class MasterView : public QWidget
 public:
     MasterView(QWidget *parent = nullptr);
     ~MasterView();
+public slots:
+    void goLoginView();
+    void goWelcomeView();
+    void goDoctorView();
+    void goDepartmentView();
+    void goPatientEditView();
+    void goPatientView();
+    void goPreviousView();
+
+private slots:
+    void on_btBack_clicked();
 
 private:
+    void pushWidgetToStackView(QWidget *widget);
+
     Ui::MasterView *ui;
+
+    WelcomeView *welcomeView;
+    DoctorView *doctorView;
+    PatientView *patientView;
+    DepartmentView *departmentView;
+    LoginView *loginView;
+    PatientEditView *patientEditView;
 };
 #endif // MASTERVIEW_H
